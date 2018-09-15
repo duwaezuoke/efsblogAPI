b34f3f05ff90e2c03de2b7fcd1ac38ab39f7b861
duwaezuoke <duwaezuoke@unomaha.edu>
2018-09-14 21:03:41 -0500

    Complete

--- efs/urls.py
+++ efs/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
 from django.contrib.auth import views
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('portfolio.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('portfolio.urls', namespace='portfolio')),
 ]
